8f82fa220e48f88eaa1d0f3186a3be6c18e172c5
Ana Clara Carneiro de Freitas <anaclara.2001@hotmail.com>
2022-05-09 19:38:35 -0300

    atualizacoes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+    private static GameManager _instance;
+    public enum GameState { MENU, GAME, PAUSE, ENDGAME };
+
+    public GameState gameState { get; private set; }
+    public int minute;
+    public float seconds;
+    public delegate void ChangeStateDelegate();
+    public static ChangeStateDelegate changeStateDelegate;
+
+    public static GameManager GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new GameManager();
+        }
+
+        return _instance;
+    }
+    private GameManager()
+    {
+     
+        minute = 5;
+        seconds = 0;
+        gameState = GameState.GAME;
+    }
+
+    private void Reset()
+    {
+       
+        minute = 2;
+        seconds = 0;
+    }
+
+    public void ChangeState(GameState nextState)
+    {
+        if ((gameState == GameState.ENDGAME || gameState == GameState.MENU) && nextState == GameState.GAME)
+            Reset();
+        gameState = nextState;
+        changeStateDelegate();
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   float _baseSpeed = 10.0f;
+
+   CharacterController characterController;
+   //Referência usada para a câmera filha do jogador
+   GameObject playerCamera;
+   //Utilizada para poder travar a rotação no angulo que quisermos.
+   float cameraRotation;
+   
+   void Start()
+   {
+       characterController = GetComponent<CharacterController>();
+       playerCamera = GameObject.Find("Main Camera");
+       cameraRotation = 0.0f;
+   }
+
+   void Update()
+   {
+       float x = Input.GetAxis("Horizontal");
+       float z = Input.GetAxis("Vertical");
+
+        //Tratando movimentação do mouse
+       float mouse_dX = Input.GetAxis("Mouse X");
+       float mouse_dY = Input.GetAxis("Mouse Y");
+
+        //Tratando a rotação da câmera
+        if(Input.GetMouseButton(0)){
+            cameraRotation += mouse_dY;
+            transform.Rotate(Vector3.up, mouse_dX);
+        }
+       cameraRotation = Mathf.Clamp(cameraRotation, -75.0f, 75.0f);
+
+       
+       Vector3 direction = transform.right * x + transform.forward * z;
+
+       characterController.Move(direction * _baseSpeed * Time.deltaTime);
+
+       
+       playerCamera.transform.localRotation = Quaternion.Euler(cameraRotation, 0.0f, 0.0f);
+       
+   }
+
+}
